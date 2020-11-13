fd668ab441e83e0ff70c48e51ac36c5943c76301
Nikita Vakula <programmistov.programmist@gmail.com>
2020-11-13 22:18:35 +0100

    WIP
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

---
+++ index.js
@@ -0,0 +1,43 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
+    try {
+        const {
+            data
+        } = await octokit.checks.create({
+            ...github.context.repo,
+            name: github.context.action,
+            head_sha: github.context.sha,
+            started_at: new Date().toISOString(),
+        });
+        console.log(JSON.stringify(data));
+        const update = await octokit.checks.update({
+            ...github.context.repo,
+            check_run_id: data.id,
+            completed_at: new Date().toISOString(),
+            conclusion: "success",
+            output: {
+                summary: "summary",
+                title: "title",
+                annotations: [
+                    {
+                        path: "./dummyfile.txt",
+                        end_line: 1,
+                        start_line: 1,
+                        annotation_level: "failure",
+                        message: "missing value"
+                    },
+                ]
+            },
+            status: "completed",
+        })
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run()
